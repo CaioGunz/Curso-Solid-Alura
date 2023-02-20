cf5510caf48039872ec3da024bb4ee31e2e95567
cbganer@gmail.com <caiobarbosa2404@hotmail.com>
2023-02-20 16:08:40 -0300

    Primeiro commit do projeto do curso de Solid

---
+++ src/br/com/caio/rh/ValidacaoException.java
@@ -0,0 +1,13 @@
+package br.com.caio.rh;
+
+
+public class ValidacaoException extends RuntimeException {
+
+
+		private static final long serialVersionUID = 1L;
+		
+		public ValidacaoException(String mensagem) {
+			super(mensagem);
+		}
+
+}
---
+++ src/br/com/caio/rh/model/Cargo.java
@@ -0,0 +1,10 @@
+package br.com.caio.rh.model;
+
+public enum Cargo {
+
+	ASSISTENTE,
+	ANALISTA,
+	ESPECIALISTA,
+	GERENTE;
+	
+}
